0b35fb14aabe52a847658fb106044aa678c6e3ab
EmirErtorer <emir1.ertorer@gmail.com>
2026-09-02 14:58:07 +0300

    update plugin description to mention the computer opponents

--- _meta.lua
+++ _meta.lua
@@ -1,7 +1,7 @@
 local _ = require("gettext")
 return {
     fullname = _("Backgammon"),
-    description = _([[Two player backgammon (tavla) on one device.
+    description = _([[Backgammon (tavla) for one or two players. Play a friend on one device, or the computer at five difficulty levels.
 
 Tap a checker to see where it can go, tap a highlighted point to move it.]]),
 }
